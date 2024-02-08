1d3b0280f6666fb28c37268de261d0f8d5e82360
Siddharth Dagar <sid.dagar0101@gmail.com>
2024-02-08 17:09:21 +0530

    install npm

--- Day01/index.js
+++ Day01/index.js
@@ -18,7 +18,7 @@ const allUsers = [
 ];
 
 for (let i = 0; i < allUsers.length; i++){
-    if (allUsers[i]["gender"] == "Male") {
+    if (allUsers[i]["gender"] == "Female") {
         console.log(allUsers[i].firstName)
     }
 }
